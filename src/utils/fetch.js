5a6117201913f44dabfd24a69e941efb99964579
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-23 14:37:50 +0100

    feature: fetch request for registerUser function

---
+++ src/utils/fetch.js
@@ -0,0 +1,20 @@
+const API_URL = "http://localhost:5001";
+
+const handleResponse = async (response) => {
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error.message || "Something went wrong");
+  }
+  return response.json();
+};
+
+export const registerUser = async (username, email, password) => {
+  const response = await fetch(`${API_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, email, password }),
+  });
+  return handleResponse(response);
+};
