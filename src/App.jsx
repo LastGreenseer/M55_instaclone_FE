9bae1a370deb1917414cfb9a3df6957853ec537e
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-25 14:36:35 +0100

    chore: setup fetch request to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
 import React, { useState } from "react";
-import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import ImageContainer from "./components/imageContainer";
 import Signup from "./components/logOrSign/signup/Signup";
-import Login from "./components/logOrSign/login/Login"
+import Login from "./components/logOrSign/login/Login";
+import "./App.css";
+import { fetchPhotos } from "./utils/unsplashFetch";
 
 function App() {
   const [showSignup, setShowSignup] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [photos, setPhotos] = useState([]);
 
   const handleChange = (e, setter) => {
     setter(e.target.value);
   };
 
-  
-
   return (
     <>
-      <Header setShowSignup={setShowSignup} setShowLogin={setShowLogin} isLoggedIn={isLoggedIn} />
+      <Header
+        setShowSignup={setShowSignup}
+        setShowLogin={setShowLogin}
+        isLoggedIn={isLoggedIn}
+      />
       <h1>{import.meta.env.VITE_COOL_TEST}</h1>
 
       {showSignup && (
         <Signup handleChange={handleChange} setShowSignup={setShowSignup} />
       )}
       {showLogin && (
-        <Login handleChange={handleChange} setShowLogin={setShowLogin} setIsLoggedIn={setIsLoggedIn} />
+        <Login
+          handleChange={handleChange}
+          setShowLogin={setShowLogin}
+          setIsLoggedIn={setIsLoggedIn}
+        />
       )}
-        <ImageContainer />
-      
+      <ImageContainer />
 
       <Footer />
     </>
