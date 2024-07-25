45bef96203a74747ac9dcaf87852b7de1999fe48
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-25 12:27:57 +0100

    welcome message when state is set to logged in

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./components/logOrSign/login/Login"
 function App() {
   const [showSignup, setShowSignup] = useState(false);
   const [showLogin, setShowLogin] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   const handleChange = (e, setter) => {
     setter(e.target.value);
@@ -18,14 +19,14 @@ function App() {
 
   return (
     <>
-      <Header setShowSignup={setShowSignup} setShowLogin={setShowLogin} />
+      <Header setShowSignup={setShowSignup} setShowLogin={setShowLogin} isLoggedIn={isLoggedIn} />
       <h1>{import.meta.env.VITE_COOL_TEST}</h1>
 
       {showSignup && (
         <Signup handleChange={handleChange} setShowSignup={setShowSignup} />
       )}
       {showLogin && (
-        <Login handleChange={handleChange} setShowLogin={setShowLogin} />
+        <Login handleChange={handleChange} setShowLogin={setShowLogin} setIsLoggedIn={setIsLoggedIn} />
       )}
         <ImageContainer />
       
